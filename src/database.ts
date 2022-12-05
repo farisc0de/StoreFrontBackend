1ff9b1d644bebe63c475fbca4d82e891beae6af5
Faris M Alotaibi <f.alotaibi@misk.org.sa>
2022-12-05 11:42:46 +0300

    Updated Database

--- src/database.ts
+++ src/database.ts
@@ -3,14 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const { DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME, ENV } = process.env
+const { DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME, DATABASE_NAME_TEST, ENV } =
+  process.env
 
 let client: Pool = new Pool()
 
 if (ENV === 'test') {
   client = new Pool({
     host: DATABASE_HOST,
-    database: DATABASE_NAME,
+    database: DATABASE_NAME_TEST,
     user: DATABASE_USER,
     password: DATABASE_PASS
   })
